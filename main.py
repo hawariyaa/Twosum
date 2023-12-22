004e0ecf3c3901f13fb2842f1ceef0110540db07
hawariyaw paulos <hawipaul308@gmail.com>
2023-12-22 13:18:32 -0800

    twosum3

--- main.py
+++ main.py
@@ -5,6 +5,7 @@
 # so here we are going to use hash-tables
 # key = index number = value
 # i store the key value after i checked the difference
+
 num = [1, 2, 3, 4]
 target = 7
 
@@ -20,4 +21,25 @@ def twosum(num, target):
 answer = twosum(num, target)
 print(answer)
 
+# when implemented in leetcode this was the code i used
+class Solution(object):
+    def twoSum(self, nums, target):
+
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+
+        """
+
+        sum = {}
+        for number in range(len(nums)):
+            diff = target - nums[number]
+            if diff in sum:  # if the diff is inside the sum, this works for only
+# searching key inside sum, it wont work if it is value inside the sum
+# so here diff(key) inside the sum
+                return [sum[diff], number]
+            else:
+                sum[nums[number]] = number
+                
 
